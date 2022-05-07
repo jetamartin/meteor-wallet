52675d6c462c2d1e27d781299175e1c7a69a5108
Jet Martin <jetamartin@gmail.com>
2022-05-07 11:46:04 -0700

    Add Galaxy to title

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,7 +5,7 @@ import { ContactList } from "./ContactList";
 export const App = () => {
   return (
     <div>
-      <h1>Meteor Wallet!</h1>
+      <h1>Meteor Wallet -- Galaxy!</h1>
       <ContactForm />
       <ContactList />
     </div>
