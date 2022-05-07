182a494c6168179db9e2b9bfb829b80487e9181c
Jet Martin <jetamartin@gmail.com>
2022-05-07 16:12:23 -0700

    add styling

--- imports/ui/ContactForm.jsx
+++ imports/ui/ContactForm.jsx
@@ -27,30 +27,54 @@ export const ContactForm  = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">
+    <form className="mt-6" onSubmit={handleSubmit} >
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
           Name
         </label>
-        <input id="name" name="name" type="text" value={ formData.name } onChange={ handleChange }/>
+        <input 
+          id="name" name="name"
+          type="text" 
+          value={ formData.name }
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          onChange={ handleChange }/>
       </div>
 
-      <div>
-        <label htmlFor="email">
+      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
           Email
         </label>
-        <input id="email" name="email" type="email" value={ formData.email } onChange={ handleChange } />
+        <input 
+          id="email" 
+          name="email" 
+          type="email" 
+          value={ formData.email } 
+          onChange={ handleChange }
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        />
       </div>
 
-      <div>
-        <label htmlFor="imageURL">
+      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700">
           ImageURL
         </label>
-        <input id="imageURL" name="imageURL" type="text" value={ formData.imageURL } onChange={ handleChange } />
+        <input 
+          id="imageURL" 
+          name="imageURL" 
+          type="text" 
+          value={ formData.imageURL } 
+          onChange={ handleChange }
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        />
+      </div>
       </div>
 
-      <div>
-        <button >Save Contact</button>
+      <div className="px-2 py-3 text-right">
+        <button 
+          className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
+          Save Contact</button>
       </div>
     </form>
   );
